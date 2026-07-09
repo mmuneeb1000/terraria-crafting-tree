4a2e68336bb3862cc2fae48e71d58198aa48053b
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-09 19:03:32 +0500

    layout adjustment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import ItemList from "./ItemList";
 import TreeList from "./TreeList";
+import HeroImg from "../assets/ui/terraria-hero-section.jpg/";
 
 function Home() {
   return (
     <>
+      <img />
       <TreeList />
     </>
   );
