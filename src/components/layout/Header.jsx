19036d01dbca9da0b1b1d4a02ba9bed3d381008e
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-12 00:18:37 +0500

    header look modified

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,16 +6,18 @@ import Sitelogo from "../../assets/logos/terraria-recipes-150px.png";
 function Header() {
   return (
     <header className="border-b border-accent w-full bg-background">
-      <div className="p-4 flex items-center justify-center">
-        <div className="text-xl font-bold">
-          <Link to="/" className="flex items-center gap-6">
-            <img src={Sitelogo} alt="Terraria Recipes" className="w-20" />
+      <div
+        className="p-2 flex flex-col lg:flex-row lg:gap-5 
+      items-center justify-between gap-2"
+      >
+        <Link to="/" className="flex items-center text-xl font-bold gap-2">
+          <img src={Sitelogo} alt="Terraria Recipes" className="w-20" />
 
-            <h1 className="text-lg font-bold">Terraria Recipes</h1>
-          </Link>
-        </div>
+          <h1 className="text-lg font-bold">Terraria Recipes</h1>
+        </Link>
+
+        <Navbar />
       </div>
-      <Navbar />
     </header>
   );
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,29 +2,36 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="flex justify-center items-center gap-5 m-2 lg:gap-8">
+    <nav
+      className="flex justify-center text-sm font-semibold items-center gap-5 mx-2 
+    lg:gap-8 lg:text-base"
+    >
       <Link
         to="/"
-        className="border-b-2 border-background font-semibold hover:border-accent active:border-accent focus:border:accent transition"
+        className="border-b-2 border-background hover:border-accent 
+        active:border-accent focus:border:accent transition"
       >
         Home
       </Link>
       <Link
         to="/trees"
-        className="border-b-2 border-background font-semibold hover:border-accent active:border-accent focus:border:accent transition"
+        className="border-b-2 border-background hover:border-accent 
+        active:border-accent focus:border:accent transition"
       >
         Trees
       </Link>
       <Link
         to="/search"
-        className="border-b-2 border-background font-semibold hover:border-accent active:border-accent focus:border:accent transition"
+        className="border-b-2 border-background hover:border-accent 
+        active:border-accent focus:border:accent transition"
       >
         Search
       </Link>
 
       <Link
         to="/about"
-        className="border-b-2 border-background font-semibold hover:border-accent active:border-accent focus:border:accent transition"
+        className="border-b-2 border-background hover:border-accent 
+        active:border-accent focus:border:accent transition"
       >
         About
       </Link>
